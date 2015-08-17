6e9086882a0f721b5c46447ed62a00a1d0212ce2
Nathaniel Olsen <NateTenn5123@Gmail.com>
2015-08-16 19:07:12 -0500

    Capitalize true.

--- bunny.py
+++ bunny.py
@@ -34,7 +34,7 @@ class Bunny(object):
         logging.info("Connecting")
 
         # Hack to send the server password. This gets queued but not sent until we connect
-        if self.config['sasl'] == true:
+        if self.config['sasl'] == True:
             sasl_username = self.config['sasl_username']
             sasl_password = self.config['sasl_password']
             self.irc.send("CAP REQ :sasl")
